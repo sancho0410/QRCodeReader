4f7480ccef485eea64b7290de3798dada89c17e8
sancho0410 <gs.tribu@gmail.com>
2017-08-23 00:09:27 +0200

    Initial commit

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour {
+
+    public Transform textMeshObject;
+
+    // Use this for initialization
+    private void Start()
+    {
+        this.textMesh = this.textMeshObject.GetComponent<TextMesh>();
+        this.OnReset();
+    }
+    public void OnScan()
+    {
+        this.textMesh.text = "scan pendant 30s";
+
+#if !UNITY_EDITOR
+    MediaFrameQrProcessing.Wrappers.ZXingQrCodeScanner.ScanFirstCameraForQrCode(
+        result =>
+        {
+          UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+          {
+            this.textMesh.text = result?.Text ?? "Non trouvé";
+          }, 
+          false);
+        },
+        TimeSpan.FromSeconds(30));
+#endif
+    }
+    public void OnReset()
+    {
+        this.textMesh.text = "Dire 'go' pour commencer !";
+    }
+
+    TextMesh textMesh;
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
